44bdbca76d67b7b3c09e5e1177b6e5782f4b6ed4
Hung Nguyen <nh.hung724@posco.net>
2023-11-23 08:51:00 +0700

    - Update Field DB column from ORDER to SORT - Update create by and update by to integer

--- ampro015-store/src/main/java/com/poscodx/odc/ampro015/store/jpo/FieldJpo.java
+++ ampro015-store/src/main/java/com/poscodx/odc/ampro015/store/jpo/FieldJpo.java
@@ -1,13 +1,18 @@
 package com.poscodx.odc.ampro015.store.jpo;
 
+import com.poscdx.odc.ampro015.domain.entity.Field;
 import com.posco.reuse.common.errorobjects.PosBaseException;
 import com.poscoict.base.share.jpo.PoscoEntityJpo;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.beans.BeanUtils;
 
 import javax.persistence.*;
 import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 @Getter
 @Setter
@@ -24,8 +29,8 @@ public class FieldJpo extends PoscoEntityJpo {
     @Column(name = "ASSET_ID")
     private int assetId;
 
-    @Column(name = "ORDER_BY")
-    private int orderBy;
+    @Column(name = "SORT")
+    private int sort;
 
     @Column(name = "NAME")
     private String name;
@@ -34,19 +39,34 @@ public class FieldJpo extends PoscoEntityJpo {
     private String value;
 
     @Column(name = "CREATE_BY")
-    private String createBy;
+    private int createBy;
 
     @Column(name = "CREATE_AT")
     private Date createAt;
 
     @Column(name = "UPDATE_BY")
-    private String updateBy;
+    private int updateBy;
 
     @Column(name = "UPDATE_AT")
     private Date updateAt;
 
     @Column(name = "DELETE_AT")
     private Date deleteAt;
+
+    public FieldJpo(Field domainEntity) {
+        BeanUtils.copyProperties(domainEntity, this);
+    }
+
+    public Field toDomain() {
+        Field domainEntity = new Field();
+        BeanUtils.copyProperties(this, domainEntity);
+        return domainEntity;
+    }
+
+    public static List<Field> toDomains(Iterable<FieldJpo> jpos) {
+        return StreamSupport.stream(jpos.spliterator(), false).map(FieldJpo::toDomain).collect(Collectors.toList());
+    }
+
     @Override
     public void validateJpo() throws PosBaseException {
 
