9bb0aff7988dcbb2f5f8a119566fe56db0c9c393
DESKTOP-GFVCSV1\User <ducnghia@posco.net>
2023-11-30 11:02:56 +0700

    update structure

---
+++ ampro015-service/src/main/java/com/poscodx/odc/ampro015/service/rest/Pme00ProjectResource.java
@@ -0,0 +1,14 @@
+package com.poscodx.odc.ampro015.service.rest;
+
+import com.poscdx.odc.ampro015.domain.entity.Pme00ProjectInfo;
+import com.poscdx.odc.ampro015.domain.spec.Pme00ProjectInfoService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/Project")
+public class Pme00ProjectResource {
+
+
+}
