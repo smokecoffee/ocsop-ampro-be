2869f73b759266fb8f8af0c6224a7355cab1ad30
Tran Xuan Long <tranxuanlonga5@gmail.com>
2023-12-01 16:55:28 +0700

    add_api_get_post_delete_meeting

--- ampro015-service/src/main/java/com/poscodx/odc/ampro015/service/rest/Pme00MeetingResource.java
+++ ampro015-service/src/main/java/com/poscodx/odc/ampro015/service/rest/Pme00MeetingResource.java
@@ -1,14 +1,44 @@
 package com.poscodx.odc.ampro015.service.rest;
 import com.poscdx.odc.ampro015.domain.entity.Pme00Meeting;
 import com.poscdx.odc.ampro015.domain.spec.Pme00MeetingService;
+import com.poscodx.odc.ampro015.service.lifecycle.ServiceLifecycler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/Meeting")
+@RequiredArgsConstructor
+@RequestMapping("/meeting")
 public class Pme00MeetingResource {
+    private final ServiceLifecycler serviceLifecycle;
 
+    @GetMapping("/listMeeting")
+    public List<Pme00Meeting> getListMeeting() {
+        return this.serviceLifecycle.requestPme00MeetingService().findAll();
+    }
 
+    @DeleteMapping("/deleteMeeting")
+    public void deleteMeeting(@RequestParam("meetingId") int meetingId) {
+        this.serviceLifecycle.requestPme00MeetingService().remove(meetingId);
+    }
 
+    @PostMapping("/addMeeting")
+    public ResponseEntity addMeeting(@RequestBody Pme00Meeting newMeeting) {
+        boolean checkValidRequest = newMeeting.getMeetingId() > 0 && newMeeting.getCd_tp_id() > 0 && newMeeting.getCreatorId() != "" && newMeeting.getRequesterId() != "" && newMeeting.getCategoryMeeting() != "" ;
+        if( checkValidRequest ) {
+            int id = newMeeting.getMeetingId();
+            Pme00Meeting findMeeting = this.serviceLifecycle.requestPme00MeetingService().find(id);
+            if (findMeeting == null) {
+                this.serviceLifecycle.requestPme00MeetingService().register(newMeeting);
+                return new ResponseEntity<>("The meeting has been created successfully", HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>("This meeting has been created", HttpStatus.BAD_REQUEST);
+            }
+        } else {
+            return new ResponseEntity<>("Not valid request", HttpStatus.BAD_REQUEST);
+        }
+    }
 }
