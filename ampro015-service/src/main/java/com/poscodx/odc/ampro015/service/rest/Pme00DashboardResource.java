d0e7edc5fb48e53b28579faa752a1038cdd03547
minh.le <minh.le@posco.net>
2023-12-11 16:53:19 +0700

    202272 | Update endpoint

---
+++ ampro015-service/src/main/java/com/poscodx/odc/ampro015/service/rest/Pme00DashboardResource.java
@@ -0,0 +1,24 @@
+package com.poscodx.odc.ampro015.service.rest;
+
+import com.poscdx.odc.ampro015.domain.entity.Pme00Dashboard;
+import com.poscdx.odc.ampro015.domain.lifecycle.ServiceLifecycle;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/dashboard")
+public class Pme00DashboardResource {
+    private final ServiceLifecycle serviceLifecycle;
+
+    public Pme00DashboardResource(ServiceLifecycle serviceLifecycle) {
+        this.serviceLifecycle = serviceLifecycle;
+    }
+
+    @GetMapping(path = "")
+    public Pme00Dashboard loadDashboard() {
+        return this.serviceLifecycle.requestLevel2Service().loadDashboard(serviceLifecycle);
+    }
+}
