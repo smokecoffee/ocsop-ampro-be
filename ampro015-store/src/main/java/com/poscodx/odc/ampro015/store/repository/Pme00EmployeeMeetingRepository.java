e37db03bdab287c58f080846e5f97ca4949dae21
DESKTOP-8CMMGF8\User <nguyenhoaison15091996@gmail.com>
2023-12-11 14:17:23 +0700

    add api delete booking meeting room

--- ampro015-store/src/main/java/com/poscodx/odc/ampro015/store/repository/Pme00EmployeeMeetingRepository.java
+++ ampro015-store/src/main/java/com/poscodx/odc/ampro015/store/repository/Pme00EmployeeMeetingRepository.java
@@ -3,14 +3,13 @@ package com.poscodx.odc.ampro015.store.repository;
 import com.poscodx.odc.ampro015.store.jpo.Pme00EmployeeMeetingJpo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface Pme00EmployeeMeetingRepository extends JpaRepository<Pme00EmployeeMeetingJpo, Integer> {
     List<Pme00EmployeeMeetingJpo> findByMeetingId(int meeting_id);
 
-    @Query(value = "DELETE \n"+
-          "FROM TB_PME00_EMPLOYEE_MEETING em \n"+
-           "WHERE em.MEETING_ID = :meetingId",nativeQuery = true)
-    void deleteAllByMeetingId(int meetingId);
+    @Query(value = "DELETE FROM TB_PME00_EMPLOYEE_MEETING  WHERE MEETING_ID = :meetingId",nativeQuery = true)
+    void deleteAllByMeetingId(@Param("meetingId") int meetingId);
 }
