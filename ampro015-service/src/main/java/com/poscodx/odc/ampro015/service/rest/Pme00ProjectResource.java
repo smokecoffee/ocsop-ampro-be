56123df0e3ded49981e2e297514ee41c2c513471
DESKTOP-6F1ULH3\User <cuongtrinhq@gmail.com>
2023-12-12 08:41:44 +0700

    merge with Tuan

--- ampro015-service/src/main/java/com/poscodx/odc/ampro015/service/rest/Pme00ProjectResource.java
+++ ampro015-service/src/main/java/com/poscodx/odc/ampro015/service/rest/Pme00ProjectResource.java
@@ -1,21 +1,14 @@
 package com.poscodx.odc.ampro015.service.rest;
-
 import com.poscdx.odc.ampro015.domain.entity.*;
 import com.poscdx.odc.ampro015.domain.lifecycle.ServiceLifecycle;
-import com.poscdx.odc.ampro015.domain.spec.Pme00ProjectInfoService;
 import com.posco.reuse.common.logging.PosLogWriterIF;
 import com.posco.reuse.common.logging.PosLogger;
 import com.poscdx.odc.ampro015.domain.entity.Pme00ProjectListDto;
-import com.poscdx.odc.ampro015.domain.lifecycle.ServiceLifecycle;
-import com.poscdx.odc.ampro015.domain.spec.Pme00ProjectInfoService;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
-import oracle.jdbc.proxy.annotation.Post;
 import org.springframework.web.bind.annotation.*;
 import com.poscdx.odc.ampro015.domain.entity.M00Task;
 import com.poscdx.odc.ampro015.domain.entity.Pme00ProjectInfo;
 import com.poscdx.odc.ampro015.domain.entity.Pme00ProjectMonitoringDto;
-import com.poscdx.odc.ampro015.domain.lifecycle.ServiceLifecycle;
 import com.poscodx.odc.ampro015.service.lifecycle.ServiceLifecycler;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -44,21 +37,19 @@ public class Pme00ProjectResource {
         return this.serviceLifecycle.requestPme00ProjectInfoService().getProjectList(serviceLifecycle, dto);
     }
 
-    private final ServiceLifecycle service;
-
-    public Pme00ProjectResource(ServiceLifecycler service) { this.service = service; }
+    public Pme00ProjectResource(ServiceLifecycler service) { this.serviceLifecycle = service; }
 
     @CrossOrigin
     @GetMapping(path = "/project_monitoring/all")
     public List<Pme00ProjectMonitoringDto> findAllProjectMonitoring() {
         List<Pme00ProjectMonitoringDto> result = new ArrayList<>();
 
-        List<Pme00ProjectInfo> projectList = this.service.requestPme00ProjectInfoService().findAll();
+        List<Pme00ProjectInfo> projectList = this.serviceLifecycle.requestPme00ProjectInfoService().findAll();
 
         if (projectList != null) {
             for (Pme00ProjectInfo pme00ProjectInfo : projectList) {
 
-                List<M00Task> taskList = this.service.requestTaskService().findAll(pme00ProjectInfo.getCdV());
+                List<M00Task> taskList = this.serviceLifecycle.requestTaskService().findAll(pme00ProjectInfo.getCdV());
 
                 Pme00ProjectMonitoringDto newObject = new Pme00ProjectMonitoringDto(pme00ProjectInfo, taskList);
 
