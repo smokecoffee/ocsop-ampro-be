0819566616208229563c476c84094f96239a8354
minh.le <minh.le@posco.net>
2024-01-08 15:44:24 +0700

    Add search announcement

--- ampro015-domain/src/main/java/com/poscdx/odc/ampro015/domain/spec/Pme00AnnouncementService.java
+++ ampro015-domain/src/main/java/com/poscdx/odc/ampro015/domain/spec/Pme00AnnouncementService.java
@@ -2,12 +2,15 @@ package com.poscdx.odc.ampro015.domain.spec;
 
 import com.poscdx.odc.ampro015.domain.entity.Pme00Announcement;
 
+import java.util.Date;
 import java.util.List;
 
 public interface Pme00AnnouncementService {
     Pme00Announcement find(int id);
 
     List<Pme00Announcement> findAll();
+
+    List<Pme00Announcement> findByConditions(String name, int type, String status, Date date);
 
     void modify(List<Pme00Announcement> entityList);
 
